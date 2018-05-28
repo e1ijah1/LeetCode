621b29bfd48542ad6aa25ae4fa65ddf1ef701d66
f1renze <f1renze@163.com>
2018-05-29 01:08:05 +0800

    5.23

--- Linked_List/easy_palindrome_linked_list.py
+++ Linked_List/easy_palindrome_linked_list.py
@@ -35,76 +35,54 @@ class ListNode:
 
 
 class Solution:
-    # 输入一串数字转换为链表, 每个节点的值都是单个数字
-    def num_to_linkedlist(self, num):
-        if not isinstance(num, int):
-            raise ValueError
-        num = str(num)
-        root = p = ListNode(0)
-        for c in num:
-            node = ListNode(int(c))
-            p.next = node
-            p = p.next
-        return root.next
-
-    def reverse_list(self, head):
-        '''
-        变动操作
-        从一个表的首端不断取下节点, 将其加入另一个表的首端, 形成反转过程
-        取下和加入都是 O(1) 操作, 总时间开销 O(n)
-        '''
-        if not head:
-            return
-        rl = None
-        p = head
-        while p:
-            tmp = p.next
-            # 将 head 引用指向下一节点(取下原来的首节点)
-            p.next = rl
-            # 将原来的首节点的下一节点指向 rl
-            rl = p
-            # 指向倒序的 list
-            p = tmp
-        # 返回反转后的链表
-        return rl
-
-    def _get_mid_point(self, head):
-        slow = fast = head
-        while fast and fast.next:
-            # 随着循环次数的增长, fast 始终比 slow 多扫描一倍的节点
-            # 1, 2; 2, 4; 3, 6;...
-            slow = slow.next
-            fast = fast.next.next
-        # 当 fast.next == None, 扫描到尽头,
-        # slow 扫描前半部分, 返回的节点为后半部分的首节点
-        return slow
-
-    def is_palindrome(self, head):
+    '''
+    75.27%
+    '''
+    def is_palindrome_final(self, head):
         '''
         :type head: ListNode
         :rtype: bool
         '''
-        second_half = self._get_mid_point(head)
-        print(second_half)
-        reversed_second = self.reverse_list(second_half)
-        print(reversed_second)
-        is_palindrome = True
-        np, rp = head, reversed_second
-        while np and rp and np is not rp:
-            if np.val != rp.val:
-                is_palindrome = False
-                break
+        # 空表和单元素链表都为回文链表
+        if not head or not head.next:
+            return True
+        p = head
+        count = 0
+        while p and p.next:
+            p = p.next
+            count += 1
+        # 计数并求得中间数
+        count = count // 2
+        middle = head
+        # 取得中间节点对象
+        while count > 0:
+            count -= 1
+            middle = middle.next
+
+        # 倒序
+        rmiddle = None
+        while middle:
+            # 暂存原来节点的下一节点对象
+            tmp = middle.next
+            # 将原来节点下一节点指向倒序链表的首节点
+            middle.next = rmiddle
+            # 完成前端插入后更新倒序链表
+            rmiddle = middle
+            # 变量指向正序链表的下一节点
+            middle = tmp
+
+        # 此时已经改变了链表, 中间节点之后断开
+        np = head
+        while np and rmiddle:
+            # 若前半部分链表与后半部分的倒序链表不同值返回 False
+            if np.val != rmiddle.val:
+                return False
             else:
                 np = np.next
-                rp = rp.next
-        # self.reverse_list(second_half)
-        return is_palindrome
+                rmiddle = rmiddle.next
+        return True
+
 
 if __name__ == '__main__':
-    s = Solution()
-    l = s.num_to_linkedlist(123456)
-    print(l)
-    print(s.is_palindrome(l))
-    print(l)
-    l = s.reverse_list(l)
-    print(l)
+    pass
+
