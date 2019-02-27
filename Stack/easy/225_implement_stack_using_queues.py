95b1e0daeb4bf4860200e680724b398aa0a9a564
F1renze <f1renze@163.com>
2019-02-27 16:11:27 +0800

    stack

---
+++ Stack/easy/225_implement_stack_using_queues.py
@@ -0,0 +1,92 @@
+
+from queue import Queue
+
+
+class QueueStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = Queue()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.data.put(x)
+        for i in range(self.data.qsize() - 1):
+            self.data.put(self.data.get())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.data.get()
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        r = self.data.get()
+        self.push(r)
+        return r
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return self.data.empty()
+
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.a = []
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.a.append(x)
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        b = self.a[-1]
+        del self.a[-1]
+        return b
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.a[-1]
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return len(self.a) == 0
+
+
+if __name__ == '__main__':
+    q = MyStack()
+    q.push(1)
+    q.push(2)
+    q.push(3)
+    print(
+        q.top(),
+        q.pop(),
+        q.top(),
+        q.pop()
+    )
--- Stack/easy/232_implement_queue_using_stacks.py
+++ Stack/easy/232_implement_queue_using_stacks.py
@@ -14,7 +14,9 @@ class StackQueue:
         self.data = [None] * n
         walk = self.head
         for k in range(self.size):
-
+            self.data[k] = old[walk]
+            walk = walk + 1 % len(old)
+        self.head = 0
 
     def push(self, x: int) -> None:
         """
@@ -22,8 +24,6 @@ class StackQueue:
         :param x:
         :return:
         """
-        if self.empty():
-            raise RuntimeError('Queue is empty!')
         location = (self.head + self.size) % len(self.data)
         self.data[location] = x
         self.size += 1
@@ -32,18 +32,86 @@ class StackQueue:
 
     def pop(self) -> int:
         """
-
+        Removes the element from in front of queue and returns that element.
         :return:
         """
+        if self.empty():
+            raise RuntimeError('Queue is empty!')
+        result = self.data[self.head]
+        self.data[self.head] = None
+        self.head = (self.head + 1) % len(self.data)
+        self.size -= 1
+        if 0 < self.size < len(self.data) // 4 and len(self.data) > 10:
+            self.resize(len(self.data) // 2)
+        return result
 
     def peek(self) -> int:
         """
-
+        Get the front element.
         :return:
         """
+        return self.data[self.head]
 
     def empty(self) -> bool:
         """
 
         :return:
         """
+        return self.size == 0
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.a = []
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: void
+        """
+        self.a.append(x)
+        print(self.a)
+        # 将末尾元素移至 first
+        self.a[0], self.a[1:] = self.a[-1], self.a[:-1]
+        print(self.a)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        print(self.a)
+        return self.a.pop()
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.a[-1]
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return len(self.a) == 0
+
+
+if __name__ == '__main__':
+    q = MyQueue()
+    q.push(1)
+    q.push(2)
+    q.push(3)
+    q.push(4)
+    print(
+        q.pop(),
+        q.pop(),
+        q.pop(),
+    )
+
